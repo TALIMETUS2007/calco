0343a909f8ee5f7210c4de6d7a415c023511f6f8
SaahBrice <saahbrice98@gmail.com>
2023-06-29 20:24:08 +0200

    ajouter une interface calculatrice

--- colca.js
+++ colca.js
@@ -8,13 +8,13 @@ let Addition = document.querySelector(".addition")
 let Soustraction = document.querySelector(".soustraction")
 let Division = document.querySelector(".division")
 let Multiplication = document.querySelector(".multiplication")
-let resultat = document.getElementById('btn3')
+let ecranResultat = document.getElementById('ecranResultat')
 let valeur1 = ""
 let valeur2 = ""
 let signe = ""
 let final = ""
 
-chat.addEventListener("input", (e) => {
+/* chat.addEventListener("input", (e) => {
     valeur1 = parseInt( e.target.value )
 })
 
@@ -40,10 +40,29 @@ Division.addEventListener("click", () => {
 Multiplication.addEventListener("click", () => {
     final = valeur1*valeur2
     rat.value = final
-})
+}) */
+
+/* 
+##########################################################################
+code utile pour detecter le button aspect-ratio--object 
+########################################################################## */
 
 
+/* ici on charge le dicumnet dans le dom */
+document.addEventListener("DOMContentLoaded", function() {
+    /* on va juste recuperer tous les buttons avect SelectAll et ca va retourner cela dans un array*/
+    var buttons = document.querySelectorAll("button");
+    /* on va marcher dans le array and utilisans for. tu peux console.log(button) pour voir sont contenue
+    ensuite on va ajouter le eventListener a tout les buttons et afficher le button qui a ete afficher.
+    le innerHTML afficher la valuer qu'il y a ici <button> variable </button> qui est soit 1,2,etc*/ 
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].addEventListener("click", function() {
+        var value = this.innerHTML;
+        console.log(value);
+        ecranResultat.value = value
+      });
+    }
+  });
 
-
-
+/* ############################################################################ */
 
